ef46e70a512695fef35f8e209305d995b3b46069
Erin Dustin <erincdustin@gmail.com>
2019-05-03 12:26:14 -0500

    added toggle functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const STORE = [
 function generateItemElement(item) {
   return `
     <li data-item-id="${item.id}">
-      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}">${item.name}</span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
@@ -23,7 +23,7 @@ function generateItemElement(item) {
 }
 
 function generateShoppingItemsString(shoppingList) {
-  console.log("generating shopping list element");
+  console.log('generating shopping list element');
 
   const items = shoppingList.map((item) => generateItemElement(item));
   return items.join('');
@@ -33,28 +33,56 @@ function renderShoppingList() {
   console.log('`renderShoppingList` ran');
   const shoppingListItemString = generateShoppingItemsString(STORE);
 
-  // isert this html string to the DOM
+  // insert this html string to the DOM
   $('.js-shopping-list').html(shoppingListItemString);
 }
 
-
-function handleNewItemSubmit() {
-  // this function will be responsible for when users add a new shopping list item
-  console.log('`handleNewItemSubmit` ran');
+function addItemToShoppingList(itemName) {
+  console.log(`Adding "${itemName}" to shopping list`);
+  STORE.push({name: itemName, checked: false});
 }
 
+function handleNewItemSubmit() {
+  //listening for submit
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    const newItemName = $('.js-shopping-list-entry').val();
+    console.log(newItemName);
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
+  });
+};
+
+function toggleCheckedForListItem(itemId) {
+  console.log('Toggling checked property for item with id ' + itemId);
+  const item = STORE.find(item => item.id === itemId);
+  item.checked = !item.checked;
+}
+
+function getItemIdFromElement(item) {
+  return $(item)
+    .closest('li')
+    .data('item-id');
+}
 
 function handleItemCheckClicked() {
   // this function will be responsible for when users click the "check" button on
   // a shopping list item.
-  console.log('`handleItemCheckClicked` ran');
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    console.log('`handleItemCheckClicked` ran');
+    //is there any way to use this instead of currentTarget?
+    const itemId = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(itemId);
+    renderShoppingList();
+  });
 }
 
 
 function handleDeleteItemClicked() {
   // this function will be responsible for when users want to delete a shopping list
   // item
-  console.log('`handleDeleteItemClicked` ran')
+  console.log('`handleDeleteItemClicked` ran');
 }
 
 // this function will be our callback when the page loads. it's responsible for
